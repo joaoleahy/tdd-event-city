176a71904240e87bce0cb67a1e26a5dbecdbc916
João Leahy <jvm.leahy@gmail.com>
2022-11-18 17:09:56 -0300

    Implementing EventController tests

---
+++ backend/src/main/java/com/devsuperior/bds02/handlers/ValidationError.java
@@ -0,0 +1,23 @@
+package com.devsuperior.bds02.handlers;
+
+import com.devsuperior.bds02.handlers.FieldMessage;
+import com.devsuperior.bds02.handlers.CustomError;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError{
+    private List<FieldMessage> fieldMessages = new ArrayList<>();
+
+    public ValidationError(Instant timestamp, Integer status, String error, String path) {
+        super(timestamp, status, error, path);
+    }
+
+    public List<FieldMessage> getFieldMessages() {
+        return fieldMessages;
+    }
+
+    public void addError(String fieldName, String message) {
+        fieldMessages.add(new FieldMessage(fieldName, message));
+    }
+}
